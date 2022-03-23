a57634564298a232d1026f94631e9c61e0ac1c82
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-23 17:52:31 +0600

    changed the pc UI

--- pages/Surahs/[id].js
+++ pages/Surahs/[id].js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import ReactAudioPlayer from "react-audio-player";
 import { AudionPlayer } from "../../components";
 
 export async function getStaticPaths() {
@@ -24,9 +23,6 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Details({ quranData }) {
-  const handleAudioPlay = () => {
-    console.log("Clicked");
-  };
   return (
     <>
       <Head>
@@ -35,55 +31,80 @@ export default function Details({ quranData }) {
         </title>
       </Head>
 
-      <div className=" p-5 rounded">
-        <h1 key={quranData.number} className="text-2xl font-semibold">
-          {quranData.name.transliteration.en} -(
-          {quranData.name.long}){" "}
-        </h1>
-
-        {/* Play button */}
-        <button
+      <div className="p-5 rounded">
+        <div
+          className="flex mt-5 flex-wrap overflow-hidden sm:-mx-1"
           key={quranData.number}
-          onClick={handleAudioPlay}
-          className="bg-[#009b5a] p-2 rounded text-white mt-2"
         >
-          Play the {quranData.name.long} ({quranData.name.transliteration.en})
-          <ReactAudioPlayer
-            controls
-            preload="auto"
+          <div
             key={quranData.number}
-            src={`https://github.com/0nahid/Surah-API/blob/main/Surah/${quranData.number}.mp3?raw=true`}
-          />
-        </button>
-        {/* Details */}
-        <ol key={quranData.number}>
-          {quranData.verses.map((item) => (
-            <li
-              key={item.number}
-              className="mt-4 bg-slate-900 p-4 rounded hover:bg-[#009b5a] list-decimal"
-            >
-              <h1 className="mb-2" key={item.number}>
-                {item.text.arab}
-              </h1>
-              <h2 key={item.number}>{item.text.transliteration?.en}</h2>
-              <AudionPlayer
-                key={item.audio.primary}
-                audioSrc={item.audio.primary}
-              />
-            </li>
-          ))}
-        </ol>
+            className="w-full overflow-hidden sm:my-1 sm:px-1 md:w-1/2 lg:w-1/2 xl:w-1/2"
+          >
+            <h1 key={quranData.number} className="text-2xl font-semibold">
+              {quranData.name.transliteration.en} -(
+              {quranData.name.long})
+            </h1>
+          </div>
+          {/* Navigate to Home */}
+          <div
+            key={quranData.number}
+            className="w-full overflow-hidden sm:my-1 sm:px-1 md:w-1/2 lg:w-1/2 xl:w-1/2"
+          >
+            <Link key={quranData.name.transliteration.id} href="/">
+              <a
+                key={quranData.name.transliteration.en}
+                className="bg-[#009B5A] p-2 rounded text-white "
+              >
+                Back to Home
+              </a>
+            </Link>
+          </div>
+        </div>
 
-        {/* Navigate to Home */}
-        <div key={quranData.number} className="mt-5">
-          <Link key={quranData.name.transliteration.id} href="/">
-            <a
-              key={quranData.name.transliteration.en}
-              className="bg-[#009B5A] p-2 rounded text-white "
+        <div
+          key={quranData.name.transliteration.en}
+          className="md:grid md:grid-cols-3 md:grid-rows-2 md:gap-2"
+        >
+          <div
+            key={quranData.name.transliteration.en}
+            className="md:row-span-2"
+          >
+            {/* Play button */}
+            <div
+              key={quranData.name.transliter}
+              className="bg-[#009b5a] p-5 m-5 rounded"
             >
-              Back to Home
-            </a>
-          </Link>
+              Play the {quranData.name.long} (
+              {quranData.name.transliteration.en})
+              <AudionPlayer
+                key={quranData.number}
+                src={`https://github.com/0nahid/Surah-API/blob/main/Surah/${quranData.number}.mp3?raw=true`}
+              />
+            </div>
+          </div>
+          <div
+            key={quranData.name.transliteration.en}
+            className="md:col-start-2 md:col-span-2"
+          >
+            {/* Details */}
+            <ol key={quranData.number}>
+              {quranData.verses.map((item) => (
+                <li
+                  key={item.number}
+                  className="mt-4 bg-slate-900 p-4 rounded hover:bg-[#009b5a] list-decimal"
+                >
+                  <h1 className="mb-2" key={item.number}>
+                    {item.text.arab}
+                  </h1>
+                  <h2 key={item.number}>{item.text.transliteration?.en}</h2>
+                  <AudionPlayer
+                    key={item.audio.primary}
+                    audioSrc={item.audio.primary}
+                  />
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
       </div>
     </>
