d13940b5bde8fe3ae1f770fd9d6cf2387ce92386
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-24 11:07:30 +0600

    changed the ui

--- pages/Surahs/[id].js
+++ pages/Surahs/[id].js
@@ -31,67 +31,57 @@ export default function Details({ quranData }) {
         </title>
       </Head>
 
-      <div
-        className="p-5  "
-        key={quranData.name.transliteration.en}
-      >
+      <div key={quranData.name.transliteration.en}>
+        {/* Name & Navigation */}
         <div
-          className="flex flex-wrap -mx-2 overflow-hidden text-center items-center bg-[#1e293b] p-5 fixed w-full -m-0 top-0 z-50 "
-          key={quranData.name.transliteration.en}
+          key={quranData.name.transliter}
+          className="flex flex-wrap -mx-2 overflow-hidden"
         >
           <div
+            key={quranData.name.transliter}
             className="my-2 px-2 w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2"
-            key={quranData.name.transliteration.en}
           >
-            <div key={quranData.number}>
-              <h1 key={quranData.number} className="text-2xl font-semibold">
-                {quranData.name.transliteration.en} -(
-                {quranData.name.long})
-              </h1>
-            </div>
+            <h1 key={quranData.number} className="text-2xl font-semibold">
+              {quranData.name.transliteration.en} -(
+              {quranData.name.long})
+            </h1>
           </div>
+
           <div
+            key={quranData.name.transliter}
             className="my-2 px-2 w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2"
-            key={quranData.name.transliteration.en}
           >
-            {/* Navigate to Home */}
-            <div key={quranData.number}>
-              <Link key={quranData.name.transliteration.id} href="/">
-                <a
-                  key={quranData.name.transliteration.en}
-                  className="bg-[#009b5a] p-10 m-10 rounded"
-                >
-                  Back to Home
-                </a>
-              </Link>
-            </div>
+            <Link key={quranData.name.transliteration.id} href="/">
+              <a
+                key={quranData.name.transliteration.en}
+                className="bg-[#009b5a] p-10 m-10 rounded"
+              >
+                Back to Home
+              </a>
+            </Link>
           </div>
         </div>
 
+        {/* Details & Play */}
+
         <div
-          key={quranData.name.transliteration.en}
-          className="md:grid md:grid-cols-3 md:grid-rows-2 md:gap-2 mt-10 mb-10 p-12 md:p-5"
+          key={quranData.name.transliter}
+          className="flex flex-wrap -mx-2 overflow-hidden"
         >
           <div
-            key={quranData.name.transliteration.en}
-            className="md:row-span-2"
+            key={quranData.name.transliter}
+            className="my-2 px-2 w-full overflow-hidden md:w-1/4 bg-[#009b5a] p-3 mt-6 md:mr-3 rounded"
           >
-            {/* Play button */}
-            <div
-              key={quranData.name.transliter}
-              className="bg-[#009b5a] p-3 mt-6 md:mr-3 rounded"
-            >
-              Play the {quranData.name.long} (
-              {quranData.name.transliteration.en})
-              <AudionPlayer
-                key={quranData.number}
-                audioSrc={`https://github.com/0nahid/Surah-API/blob/main/Surah/${quranData.number}.mp3?raw=true`}
-              />
-            </div>
+            Play the {quranData.name.long} ({quranData.name.transliteration.en})
+            <AudionPlayer
+              key={quranData.number}
+              audioSrc={`https://github.com/0nahid/Surah-API/blob/main/Surah/${quranData.number}.mp3?raw=true`}
+            />
           </div>
+
           <div
-            key={quranData.name.transliteration.en}
-            className="md:col-start-2 md:col-span-2"
+            key={quranData.name.transliter}
+            className="my-2 px-2 w-full overflow-hidden md:w-1/4"
           >
             {/* Details */}
             <ol key={quranData.number}>
