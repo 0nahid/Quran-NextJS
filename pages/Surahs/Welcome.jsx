3895d41c2b1f4aa105be08a6d7da6a76edc94b32
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-24 10:48:06 +0600

    updated the api on full audio

--- pages/Surahs/Welcome.jsx
+++ pages/Surahs/Welcome.jsx
@@ -1,7 +1,10 @@
 export default function Welcome() {
   return (
     <>
-      <h1 className="text-3xl font-semibold p-3 rounded bg-[#009b5a] "> القرآن al-Qurʾān</h1>
+      <h1 className="text-3xl font-semibold p-3 rounded bg-[#009b5a] ">
+        {" "}
+        القرآن al-Qurʾān
+      </h1>
     </>
   );
 }
